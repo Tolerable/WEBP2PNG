4d31a352f1dd56106231a727bad585cc0f7bcb48
Tolerable <2303210+Tolerable@users.noreply.github.com>
2024-05-19 19:13:37 -0400

    Script uploaded

---
+++ WEBP2PNG.py
@@ -0,0 +1,10 @@
+# Ignore Python bytecode files
+*.py[cod]
+
+# Ignore specific scripts, but not the main one
+WEBP2PNG-*.py
+
+# Ignore converted and original images directories
+CONVERTED/
+ORIGINALS/
+
